31d6700e24b09a28cbe687fa2e2fc08dffbe6c9c
kkohler2 <kenkohler@hotmail.com>
2020-08-18 13:05:36 -0500

    Initial release.

---
+++ VSDiff/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace VSDiff
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.DefaultExt = "*.*";
+            openFileDialog.Title = "First File";
+            openFileDialog.Filter = "Text Documents|*.txt|C# Files|*.cs|All Files|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string firstFile = openFileDialog.FileName;
+                openFileDialog.Title = "Second File";
+                openFileDialog.FileName = string.Empty;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string secondFile = openFileDialog.FileName;
+                    if (firstFile.CompareTo(secondFile) != 0)
+                    {
+                        // devenv.exe /diff list1.txt list2.txt
+                        string executable = ConfigurationManager.AppSettings["DevStudio"];
+                        Process.Start(executable, $"/diff \"{firstFile}\" \"{secondFile}\"");
+                    }
+                }
+            }
+        }
+    }
+}
